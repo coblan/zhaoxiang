ce8ed14d8c50f5ba31335f4282e6bb42391511c3
coblan <coblan163.com>
2018-02-05 23:23:34 +0800

    auto commit

--- src/hello/engin_menu.py
+++ src/hello/engin_menu.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 from helpers.director.engine import BaseEngine,page,fa,page_dc
+from helpers.maintenance.update_static_timestamp import js_stamp
 
 class PcMenu(BaseEngine):
     url_name='zhaoxiang'
@@ -16,5 +17,12 @@ class PcMenu(BaseEngine):
              ]},
         {'label':'GIS区域','url':page('geoinfo.blockpolygon'),'icon':fa('fa-map-o')},
     ]
+    
+    def custome_ctx(self, ctx):
+        ctx['js_stamp']=js_stamp
+        ctx['table_fun_config'] ={
+            'detail_link': '详情', #'<i class="fa fa-info-circle" aria-hidden="true" title="查看详情"></i>'#,
+        }
+        return ctx      
 
 PcMenu.add_pages(page_dc)
--- src/inspector/views.py
+++ src/inspector/views.py
@@ -1,3 +1,5 @@
+# encoding:utf-8
+from __future__ import unicode_literals
 from django.shortcuts import render,HttpResponse
 from .sango.inspector import InspectorCaseConnecter
 from django.utils.timezone import datetime
@@ -9,10 +11,10 @@ def inspector_case(request,code):
     # today_str = unicode(today)    
     # case = InspectorCaseConnecter(today_str, today_str, code)
     value = get_value('inspector_case','[]')
-    ls = json.loads(value)
+    ls = json.loads(value) or [['没有数据'],]
     case_list=[]
     case_list.append(ls[0])
-    for row in ls:
+    for row in ls[1:]:
         if row[-3]==code:
             case_list.append(row)
 
