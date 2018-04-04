d48a9e1d1abe51a033a9d337c5d19993427ded0c
he_yulin <he_yulin@163.com>
2018-04-04 14:21:18 +0800

    发送到lt服务器

---
+++ src/settings/lt_server.py
@@ -0,0 +1,16 @@
+from base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'zhaoxiang_gis',
+        'USER': 'root',
+        'PASSWORD': 'root533',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432', 
+    },
+}
+
+
+XUNCHA_HOST = "http://10.235.80.249:8199"
+XUNCHA_HOST = 'http://12.110.185.17:8199'
