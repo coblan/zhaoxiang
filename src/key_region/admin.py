a1dffb733aab10657905aaaa99d3677c13891667
coblan <coblan163.com>
2018-04-26 01:09:55 +0800

    改进UI

--- src/key_region/admin.py
+++ src/key_region/admin.py
@@ -1,3 +1,6 @@
+# encoding:utf-8
+from __future__ import unicode_literals
+
 from django.contrib import admin
 from helpers.director.shortcut import page_dc
 from alg import polygon2circle
@@ -8,6 +11,9 @@ class Forcast(object):
     template='key_region/forcast.html'
     def __init__(self,*args,**kw):
         pass
+    
+    def get_label(self,prefer=None):
+        return '巡查区域预测'
     
     def get_context(self):
         xun_sys = XunCha()
