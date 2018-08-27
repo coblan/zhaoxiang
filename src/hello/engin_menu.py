8dd0948b9d5b1c41bf0bddc26c6aa544b301cbab
coblan <coblan163.com>
2018-08-28 00:58:24 +0800

    围栏告警实时性

--- src/hello/engin_menu.py
+++ src/hello/engin_menu.py
@@ -27,7 +27,7 @@ class PcMenu(BaseEngine):
         {'label':'案件对比','url':page('case_cmp.duchacase'),'icon':fa('fa-search')},
         {'label':'电子围栏','icon':fa('fa-object-group'),
              'submenu':[
-                    {'label':'围栏告警','url':page('dianzi_weilan.warning')},
+                    #{'label':'围栏告警','url':page('dianzi_weilan.warning')},
                     {'label':'实时围栏告警','url':page('dianzi_weilan.realtime_warning')},
 
                     {'label':'围栏信息','url':page('dianzi_weilan.groupweilanrel')},
