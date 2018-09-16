32a4b913d5b73e29f58cff414a4a1f328b1851c4
he_yulin <he_yulin@163.com>
2018-09-16 12:32:42 +0800

    debug

--- src/inspector/management/commands/case.py
+++ src/inspector/management/commands/case.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
     
     def _req_first(self):
         today = datetime.now().date()
-        today_str = unicode(today)
+        today_str = str(today)
         
         headers={
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.4549.400 QQBrowser/9.7.12900.400',
